f760af57993611dc811f06ade742868be8b196b0
Mateusz Michalski <michalskimateusz@outlook.com>
2021-01-05 18:10:30 +0100

    add basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eius iste perspiciatis placeat voluptas et, modi non velit neque sequi vitae ad quas ipsam quidem consequatur veritatis possimus, nobis quaerat!',
+            price
         })
         await camp.save();
     }
